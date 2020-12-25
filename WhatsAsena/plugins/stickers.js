8b198cf5bb97ec9506e2fca6aa71b119f967e028
ananthu_ <ananthu@atemon.com>
2020-12-25 20:03:19 +0530

    bot

--- WhatsAsena/plugins/stickers.js
+++ WhatsAsena/plugins/stickers.js
@@ -16,7 +16,7 @@ const cwebp = require('cwebp-bin');
 const Language = require('../language');
 const Lang = Language.getString('sticker');
 
-Asena.addCommand({pattern: 'sticker', fromMe: true, desc: Lang.STICKER_DESC}, (async (message, match) => {    
+Asena.addCommand({pattern: 'sticker', fromMe: false, desc: Lang.STICKER_DESC}, (async (message, match) => {    
     if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);
     var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
     var location = await message.client.downloadAndSaveMediaMessage({
